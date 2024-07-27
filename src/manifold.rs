a3071dc1d345deb2459e5a0901d00f8533d3c70e
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2024-07-27 11:23:04 +0200

    Add Highlight trait and use Arc for Highlighter
    - Implement Highlight trait for Highlighter
    - Update Manifold and ManifoldBuilder to use Arc<Box<dyn Highlight>>

--- src/manifold.rs
+++ src/manifold.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::sync::Arc;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 enum HighlighterType {
@@ -12,9 +13,24 @@ struct Highlighter {
     colors: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+// Define the Highlight trait
+pub trait Highlight {
+    fn apply(&self, input: String) -> String;
+}
+
+// Implement the Highlight trait for Highlighter
+impl Highlight for Highlighter {
+    fn apply(&self, input: String) -> String {
+        // Example implementation of the apply method
+        format!(
+            "Applied {:?} highlighter with colors: {:?}",
+            self.highlighter_type, self.colors
+        )
+    }
+}
+
 pub struct Manifold {
-    highlighters: Vec<Highlighter>,
+    highlighters: Vec<Arc<Box<dyn Highlight>>>,
 }
 
 impl Manifold {
@@ -33,13 +49,15 @@ impl Manifold {
     }
 
     // Method to set highlighters
-    fn with_highlighters(mut self, highlighters: Vec<Highlighter>) -> Self {
+    fn with_highlighters(mut self, highlighters: Vec<Arc<Box<dyn Highlight>>>) -> Self {
         self.highlighters = highlighters;
         self
     }
 
     pub fn apply(self, text: String) -> String {
-        "".to_string()
+        self.highlighters
+            .into_iter()
+            .fold(text, |acc, highlighter| highlighter.apply(acc))
     }
 }
 
@@ -51,7 +69,7 @@ impl Default for Manifold {
 }
 
 struct ManifoldBuilder {
-    highlighters: Vec<Highlighter>,
+    highlighters: Vec<Arc<Box<dyn Highlight>>>,
     used_highlighters: HashSet<HighlighterType>,
     error: Option<String>,
 }
@@ -61,10 +79,10 @@ impl ManifoldBuilder {
         if self.used_highlighters.contains(&HighlighterType::Foo) {
             self.error = Some("Highlighter 'Foo' already added!".to_string());
         } else {
-            self.highlighters.push(Highlighter {
+            self.highlighters.push(Arc::new(Box::new(Highlighter {
                 highlighter_type: HighlighterType::Foo,
                 colors: None,
-            });
+            })));
             self.used_highlighters.insert(HighlighterType::Foo);
         }
         self
@@ -74,10 +92,10 @@ impl ManifoldBuilder {
         if self.used_highlighters.contains(&HighlighterType::Bar) {
             self.error = Some("Highlighter 'Bar' already added!".to_string());
         } else {
-            self.highlighters.push(Highlighter {
+            self.highlighters.push(Arc::new(Box::new(Highlighter {
                 highlighter_type: HighlighterType::Bar,
                 colors: Some(colors),
-            });
+            })));
             self.used_highlighters.insert(HighlighterType::Bar);
         }
         self
@@ -100,11 +118,13 @@ fn main() {
         .build();
 
     match result {
-        Ok(manifold) => println!("{:?}", manifold),
+        Ok(manifold) => {
+            let output = manifold.apply("Some text".to_string());
+            println!("Output: {:?}", output);
+        }
         Err(e) => println!("Error: {:?}", e),
     }
 
     // Using the default method to create a Manifold
     let default_manifold = Manifold::default();
-    println!("{:?}", default_manifold);
 }
